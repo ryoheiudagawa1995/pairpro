c8ffe9bc1a81f0b11cd4b0c05063ffd62697a3c4
ryohei udagawa <r.udagawa1995@gmail.com>
2020-05-27 14:09:01 +0900

    update: teamの削除機能制限

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -39,8 +39,10 @@ class TeamsController < ApplicationController
   end
 
   def destroy
-    @team.destroy
-    redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+    if current_user == @team.owner
+      @team.destroy
+      redirect_to teams_url, notice: I18n.t('views.messages.delete_team')
+    end
   end
 
   def dashboard
